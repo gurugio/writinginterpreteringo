ecb5fe7ec05f22c8978aef1888ac63ec9d6e14cf
Gioh Kim <gurugio@l.com>
2025-06-04 20:51:38 +0900

    on going: ch01

---
+++ token/token.go
@@ -0,0 +1,43 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier and Literal
+	IDENT = "IDENT" // foobar, x, y, ...
+	INT   = "INT"   // 1234
+
+	// Operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
