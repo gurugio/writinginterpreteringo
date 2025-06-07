f0d50e683923d15fede5b5b05c67df83c0d7797b
Gioh Kim <gurugio@a.com>
2025-06-07 16:35:45 +0900

    on going: ch02: parse let statement

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,6 +18,7 @@ let foobar = 818181;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -40,6 +41,39 @@ let foobar = 818181;
 			return
 		}
 	}
+}
+
+func TestLetStatementFailure(t *testing.T) {
+	input := `
+let x 5;
+let = 10;
+let 818181;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	errors := p.Errors()
+	if len(errors) == 3 {
+		return
+	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
